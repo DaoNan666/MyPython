3d17569f527d6a2ff8d889445d719017a2969379
DaoNan666 <1940811706@qq.com>
2022-04-05 19:09:13 +0800

    D11

---
+++ day11.py
@@ -0,0 +1,30 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022/4/5
+# @Author : 道南
+# @File : day11.py
+# @Software : PyCharm
+
+#  以上注释在 settings 中 的 editor 中编辑
+
+# # 格式化输出
+# age = 18
+# print("我的年龄是: %d" % age)
+# print("我的名字是: %s，我的国籍是: %s" % ("道南", "China"))
+# print("aaa", "bbb", "ccc")
+# print("www", "baidu", "com", sep=".")
+
+# password = input("请输入密码：")
+# print("刚刚输入的密码是：", password)
+
+# a = 18
+# print(type(a))  # 查看变量类型
+#
+# # 强制类型转换
+# a = int("123")
+# b = a + 100
+# print(b)
+
+# 生成随机数
+import random
+a = random.randint(1, 100)
+print(a)
